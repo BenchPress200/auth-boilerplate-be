d61f25e1e99bb613ec13bb111f719fbcc88bce85
BenchPress200 <jms0538@naver.com>
2024-07-11 13:56:05 +0900

    feat: add controller for join feature

--- src/main/java/com/auth/boilerplate/user/controller/UserController.java
+++ src/main/java/com/auth/boilerplate/user/controller/UserController.java
@@ -1,16 +1,24 @@
 package com.auth.boilerplate.user.controller;
 
 import com.auth.boilerplate.common.dto.CommonResponse;
+import com.auth.boilerplate.common.handler.ResponseHandler;
+import com.auth.boilerplate.user.dto.JoinRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/api/v1/")
 @RequiredArgsConstructor
 public class UserController {
-    @RequestMapping("/join")
-    public CommonResponse<?> join() {
 
+    private final UserService userService;
+
+    @RequestMapping("/join")
+    public CommonResponse<?> join(@RequestBody JoinRequest joinRequest) {
+        userService.join(joinRequest);
+        return ResponseHandler.handleResponse(HttpStatus.CREATED);
     }
 }
---
+++ src/main/java/com/auth/boilerplate/user/dto/JoinRequest.java
@@ -0,0 +1,6 @@
+package com.auth.boilerplate.user.dto;
+
+public class JoinRequest {
+    private String email;
+    private String password;
+}
